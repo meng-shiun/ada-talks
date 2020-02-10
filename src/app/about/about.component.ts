d73ddc238f673a35787aebf6fccfa523c2bf3ee1
meng <lin.mengshiun@gmail.com>
2020-02-10 15:39:41 +0100

    implement about data

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IAbout } from '../shared/interfaces/about';
+import { AboutService } from './about.service';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +10,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  allAbout$: Observable<IAbout[]>;
+
+  constructor(private aboutService: AboutService) { }
 
   ngOnInit() {
+    this.allAbout$ = this.aboutService.getAllAbout();
   }
 
 }
--- src/app/data/about.ts
+++ src/app/data/about.ts
@@ -3,22 +3,22 @@ import { IAbout } from '../shared/interfaces/about';
 export const aboutData: IAbout[] = [
   {
     id: 101,
-    title: 'Lueilwitz Group',
-    description: 'werhwekjrhwekjrhwekjrwehk'
+    title: 'Our values',
+    description: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet'
   },
   {
     id: 102,
-    title: 'Frami-Ledner',
-    description: 'werhwekjrhwekjrhwekjrwehk'
+    title: 'History',
+    description: 'consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur'
   },
   {
     id: 103,
-    title: 'Ledner-Bergstrom',
-    description: 'werhwekjrhwekjrhwekjrwehk'
+    title: 'Structure',
+    description: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.'
   },
   {
     id: 104,
-    title: 'Wilderman, Armstrong and Bashirian',
-    description: 'werhwekjrhwekjrhwekjrwehk'
+    title: 'Vacancies',
+    description: 'On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will,'
   }
 ];
